6bb1cca5ec911951635eba807ae4e70b32b5f184
kariz-dev <kariz-dev>
2021-06-19 15:07:23 +0700

    create a Link

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 // Step 1: import react
 import * as React from "react"
+import { Link } from "gatsby"
 
 // Step 2: Define a component
 const IndexPage = () => {
@@ -7,6 +8,7 @@ const IndexPage = () => {
     <main>
       <title>Home Page</title>
       <h1>Welcome to my Gatsby site!</h1>
+      <Link to="/about">About</Link>
       <p>I am making this by following the gatsby tutorial.</p>
     </main>
   )
