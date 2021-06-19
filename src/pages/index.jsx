d3e972e3fbb7007d71f586f67fbcb193ac747ce6
kariz-dev <muhammad.rizwan76146@gmail.com>
2021-06-19 17:11:44 +0700

    create a reusable layout component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,13 @@
 // Step 1: import react
 import * as React from "react"
-import { Link } from "gatsby"
+import Layout from '../components/layout'
 
 // Step 2: Define a component
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to="/about">About</Link>
-      <p>I am making this by following the gatsby tutorial.</p>
-    </main>
+    <Layout pageTitle='Home Page'>
+      <p>I'm  making this by following the Gatsby Tutorial</p>
+    </Layout>
   )
 }
 
