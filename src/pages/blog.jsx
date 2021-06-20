8b374093da71cbacdeae73566261cc046e24ab9f
kariz-dev <muhammad.rizwan76146@gmail.com>
2021-06-20 18:02:08 +0700

    pull the list of post filenames into the blog page

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,12 +1,34 @@
 import * as React from 'react'
+// import the graphql from the gatsby package
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const BlogPage = () => {
+// render the filename for each of your posts.
+const BlogPage = ({data}) => {
   return (
     <Layout pageTitle='My blog post'>
-      <p>My cool post will go in here</p>
+      <ul>
+        {
+          data.allFile.nodes.map(node => (
+            <li key={node.name}>
+              {node.name}
+            </li>
+          ))
+        }
+      </ul>
     </Layout>
   )
 }
+
+// define and export the page query. copy over the query built in graphql
+export const query = graphql `
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export default BlogPage
