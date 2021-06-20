9e2683bbd925daffe24e2da59a9af70f294bf76d
kariz-dev <muhammad.rizwan76146@gmail.com>
2021-06-20 17:19:10 +0700

    create a blog page and create some MDX blog posts

---
+++ src/pages/blog.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const BlogPage = () => {
+  return (
+    <Layout pageTitle='My blog post'>
+      <p>My cool post will go in here</p>
+    </Layout>
+  )
+}
+
+export default BlogPage
